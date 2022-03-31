d5cb8fd4717d7b40bfc58e5cf942197f279efd6e
erkelost <1256029807@qq.com>
2022-03-31 17:11:49 +0800

    chore(commit): preinstall lint

--- scripts/prelint/preinstall.js
+++ scripts/prelint/preinstall.js
@@ -1,5 +1,11 @@
-if (!/pnpm/.test(process.env.npm_execpath || '')) {
-    console.warn("\u001B[33mThis repository requires using pnpm as the package manager " +
-        " for scripts to work properly.\u001B[39m\n");
-    process.exit(1);
+try {
+  if (!/pnpm/.test(process.env.npm_execpath || '')) {
+    throw new Error(
+      '\u001B[33m [JZZX CONTROL WARNING]: This repository requires using pnpm as the package manager ' +
+        ' for scripts to work properly. 此存储库需要使用pnpm作为包管理器\u001B[39m\n'
+    )
+  }
+} catch (e) {
+  console.log(e.message)
+  process.exitCode = 1
 }

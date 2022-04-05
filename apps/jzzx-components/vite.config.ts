7fd0eafdafbcad41fef6d3c0f857bcc5b6218351
erkelost <1256029807@qq.com>
2022-04-05 18:08:40 +0800

    chore(layout): ✨✨✨ layout build with jzzx-layout

---
+++ apps/jzzx-components/vite.config.ts
@@ -0,0 +1,49 @@
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+// import Unocss from 'unocss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    extensions: ['.tsx', '.vue', '.jsx', '.ts', '.js'],
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  optimizeDeps: {
+    exclude: ['vue-demi']
+  },
+  build: {
+    lib: {
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+      name: 'SoybeanAdminLayout',
+      fileName: 'index'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      include: ['./src/index.ts', './src/index.vue', './src/volar.d.ts'],
+      beforeWriteFile(filePath, content) {
+        return {
+          filePath: filePath.replace('/dist/src/', '/dist/'),
+          content
+        }
+      }
+    })
+    // Unocss()
+  ]
+})

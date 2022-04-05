5dcc8da32bf8d395bc054102f978967c04a52293
erkelost <1256029807@qq.com>
2022-04-05 20:22:05 +0800

    chore(layout): ✨ ✨ ✨ ✨uncss构建lib报错 先给pro环境销毁插件维护测试

--- apps/jzzx-layout/vite.config.ts
+++ apps/jzzx-layout/vite.config.ts
@@ -4,46 +4,50 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-// import Unocss from 'unocss/vite'
+import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    extensions: ['.tsx', '.vue', '.jsx', '.ts', '.js'],
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  optimizeDeps: {
-    exclude: ['vue-demi']
-  },
-  build: {
-    lib: {
-      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
-      name: 'SoybeanAdminLayout',
-      fileName: 'index'
+export default defineConfig(async (env) => {
+  console.log(env.mode)
+
+  return {
+    resolve: {
+      extensions: ['.tsx', '.vue', '.jsx', '.ts', '.js'],
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
     },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
+    optimizeDeps: {
+      exclude: ['vue-demi']
+    },
+    build: {
+      lib: {
+        entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
+        name: 'SoybeanAdminLayout',
+        fileName: 'index'
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
         }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    dts({
-      include: ['./src/index.ts', './src/index.vue', './src/volar.d.ts'],
-      beforeWriteFile(filePath, content) {
-        return {
-          filePath: filePath.replace('/dist/src/', '/dist/'),
-          content
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      dts({
+        include: ['./src/index.ts', './src/index.vue', './src/volar.d.ts'],
+        beforeWriteFile(filePath, content) {
+          return {
+            filePath: filePath.replace('/dist/src/', '/dist/'),
+            content
+          }
         }
-      }
-    })
-    // Unocss()
-  ]
+      }),
+      env.mode === 'development' ? Unocss() : null
+    ]
+  }
 })

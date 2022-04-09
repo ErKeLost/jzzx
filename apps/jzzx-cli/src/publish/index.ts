bdf7ffdd15da06898139b1940a78d0ed8ecb873f
erkelost <1256029807@qq.com>
2022-04-09 18:02:31 +0800

    feat(publish): cli update

---
+++ apps/jzzx-cli/src/publish/index.ts
@@ -0,0 +1,11 @@
+import { DefaultLogger, taskPre, runAsync } from '../utils';
+
+/**
+ * 发布至npm
+ */
+export async function _publishNpm() {
+  const spinner = new DefaultLogger(taskPre('发布', 'start'));
+  await runAsync('npm publish --access=public');
+  spinner.succeed(taskPre('发布', 'end'));
+  return true;
+}

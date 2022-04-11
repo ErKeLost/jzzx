f079806a56a688071f528600e3f2c8afb1e3e0f0
erkelost <1256029807@qq.com>
2022-04-11 21:12:30 +0800

    feat(layout): feat export name type

--- apps/jzzx-layout/src/index.ts
+++ apps/jzzx-layout/src/index.ts
@@ -1,45 +1,45 @@
-import type { App } from 'vue-demi';
+import type { App } from 'vue-demi'
 import AdnyLayout from './index.vue'
 
 function install(app: App) {
-  app.component('AdnyLayout', AdnyLayout);
+  app.component('AdnyLayout', AdnyLayout)
 }
 
-AdnyLayout.install = install;
-
+AdnyLayout.install = install
+AdnyLayout.name = 'AdnyLayout'
 export interface LayoutProps {
   /** 布局模式 */
-  mode?: 'vertical' | 'horizontal';
+  mode?: 'vertical' | 'horizontal'
   /** 最小宽度 */
-  minWidth?: number;
+  minWidth?: number
   /** 头部可见 */
-  headerVisible?: boolean;
+  headerVisible?: boolean
   /** 头部高度 */
-  headerHeight?: number;
+  headerHeight?: number
   /** 标签可见 */
-  tabVisible?: boolean;
+  tabVisible?: boolean
   /** 标签页高度 */
-  tabHeight?: number;
+  tabHeight?: number
   /** 固定头部和标签 */
-  fixedHeaderAndTab?: boolean;
+  fixedHeaderAndTab?: boolean
   /** 底部可见 */
-  footerVisible?: boolean;
+  footerVisible?: boolean
   /** 底部高度 */
-  footerHeight?: number;
+  footerHeight?: number
   /** 固定底部 */
-  fixedFooter?: boolean;
+  fixedFooter?: boolean
   /** 侧边可见 */
-  siderVisible?: boolean;
+  siderVisible?: boolean
   /** 侧边栏高度 */
-  siderWidth?: number;
+  siderWidth?: number
   /** 侧边栏折叠状态的高度 */
-  siderCollapsedWidth?: number;
+  siderCollapsedWidth?: number
   /** 侧边栏折叠状态 */
-  siderCollapse?: boolean;
+  siderCollapse?: boolean
   /** 动画过渡时间 */
-  transitionDuration?: number;
+  transitionDuration?: number
   /** 动画过渡速度曲线 */
-  transitionTimingFunction?: string;
+  transitionTimingFunction?: string
 }
 
-export default AdnyLayout;
+export default AdnyLayout

a679aeb698342ae5737cbf1ba83e0abeaf2750a7
erkelost <1256029807@qq.com>
2022-04-25 13:45:07 +0800

    chore(cli): add command with test🤝🤝🤝

--- apps/jzzx-cli/src/shared/constant.ts
+++ apps/jzzx-cli/src/shared/constant.ts
@@ -1,8 +1,19 @@
+const figlet = require('figlet');
+import figlet from 'figlet'
 export const PKG = require('../../package.json')
 
 export const JZZX_VERSION = PKG.version
 export const JZZX_NAME = PKG.name
-export const VALUE_ONLINE = `\n ██      ██   █████╗   ██║        ██║   ██╗   ██████╗        ██████╗   ███╗   ██╗  ██║        ████████╗  ███╗   ██╗   ██████╗\n ╚██    ██   ██╔══██╗  ██║        ██║   ██║  ███╔══██╗      ██╔═══██╗  ████╗  ██║  ██║        ╚══██╔══╝  ████╗  ██║  ███╔══██╗\n  ╚██  ██    ███████║  ██║        ██║   ██║  ████████╝      ██║   ██║  ██╔██╗ ██║  ██║           ██║     ██╔██╗ ██║  ████████╝\n   ╚████     ██╔══██║  ██╚═════╗  ██║   ██║  ███╚═══╗       ██║   ██║  ██║╚██╗██║  ██╚═════╗     ██╚══╗  ██║╚██╗██║  ███╚═══╗\n    ╚██      ██║  ██║  ████████║  ╚██████╔╝   ██████║       ╚██████╔╝  ██║ ╚████║  ████████║  ████████║  ██║ ╚████║   ██████║\n     ╚╝      ╚═╝  ╚═╝  ╚═══════╝   ╚═════╝    ╚═════╝        ╚═════╝   ╚═╝  ╚═══╝  ╚═══════╝  ╚═══════╝  ╚═╝  ╚═══╝   ╚═════╝`
+export const VALUE_ONLINE =
+  '\r\n' +
+  figlet.textSync('VALUE ONLINE', {
+    font: '3D-ASCII',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 200,
+    whitespaceBreak: true
+  })
+
 export const VERSION = `\n\t\t\t\t\tPublished${
   PKG.version
 }Build @ Value Online.com\n\t\t\t\t\tBuild date: ${new Date().getFullYear()}-VALUE ONLINE`

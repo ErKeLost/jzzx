5857089fb68effc752aaf1ddd91d27b4df605be5
erkelost <1256029807@qq.com>
2022-05-04 08:46:00 +0800

    chore(colorpicker): new add colorpicker components

---
+++ apps/jzzx-colorpicker/src/test/App.vue
@@ -0,0 +1,9 @@
+<template>
+  {{ color }}
+  <d-color-picker v-model="color"></d-color-picker>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue-demi'
+import DColorPicker from '../color-picker/src/color-picker'
+const color = ref('fba')
+</script>

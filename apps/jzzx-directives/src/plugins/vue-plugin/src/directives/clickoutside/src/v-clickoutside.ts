79fb2e7ded9d10fc31c97a47c1229e7459c8fe7c
erkelost <1256029807@qq.com>
2022-06-10 14:30:59 +0800

    chore(directives): version  update

--- apps/jzzx-directives/src/plugins/vue-plugin/src/directives/clickoutside/src/v-clickoutside.ts
+++ apps/jzzx-directives/src/plugins/vue-plugin/src/directives/clickoutside/src/v-clickoutside.ts
@@ -2,6 +2,7 @@ export const clickoutside = (el: HTMLElement, value: any) => {
   document.addEventListener(
     'click',
     (e: any) => {
+      console.log(e.target)
       !el.contains(e.target) && value()
     },
     false

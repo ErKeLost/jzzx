294b843897186d40b4a1ca85b71c356663d1b47e
erkelost <1256029807@qq.com>
2022-06-23 14:17:01 +0800

    chore(directives): clickoutside 重构

--- apps/jzzx-directives/src/plugins/vue-plugin/src/directives/clickoutside/src/v-clickoutside.ts
+++ apps/jzzx-directives/src/plugins/vue-plugin/src/directives/clickoutside/src/v-clickoutside.ts
@@ -2,7 +2,6 @@ export const clickoutside = (el: HTMLElement, value: any) => {
   document.addEventListener(
     'click',
     (e: any) => {
-      console.log(e.target)
       !el.contains(e.target) && value()
     },
     false
